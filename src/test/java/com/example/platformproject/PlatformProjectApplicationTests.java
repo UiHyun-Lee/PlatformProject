d869fb8d9a66df01364fa1dbbc515af932a82fc3
uihyunlee <deutschluh@gmail.com>
2025-03-14 17:39:22 +0100

    initial commit

---
+++ src/test/java/com/example/platformproject/PlatformProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platformproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
